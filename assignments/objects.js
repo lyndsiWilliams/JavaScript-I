203b549da79c9debfab19dae37bbc60b4f29f4b5
lyndsiWilliams <kcatgirl@gmail.com>
2019-10-14 14:37:26 -0500

    Completed challenge one on 'objects.js'

--- assignments/objects.js
+++ assignments/objects.js
@@ -11,14 +11,48 @@
 
 // Example format of an intern object: 1, examples@you.edu, Example, F
 const example = {
-  id: 0,
-  name: "Example",
-  email: "examples@you.edu",
-  gender: "F",
+    id: 0,
+    name: "Example",
+    email: "examples@you.edu",
+    gender: "F",
 }
 
 // Write your intern objects here:
 
+const hire1 = {
+    id: 1,
+    name: "Mitzi",
+    email: "mmelloy0@psu.edu",
+    gender: "F",
+}
+
+const hire2 = {
+    id: 2,
+    name: "Kennan",
+    email: "kdiben1@tinypic.com",
+    gender: "M",
+}
+
+const hire3 = {
+    id: 3,
+    name: "Keven",
+    email: "kmummery2@wikimedia.org",
+    gender: "M",
+}
+
+const hire4 = {
+    id: 4,
+    name: "Gannie",
+    email: "gmartinson3@illinois.edu",
+    gender: "M",
+}
+
+const hire5 = {
+    id: 5,
+    name: "Antonietta",
+    email: "adaine5@samsung.com",
+    gender: "F",
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
